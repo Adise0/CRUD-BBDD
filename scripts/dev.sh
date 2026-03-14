634b103069ae9dc6b7e3761e0004148acc254c52
Adise <[admin@pauibanez.me](mailto:admin@pauibanez.me)>
2026-03-14 14:58:45 +0100

    initial comit

---
+++ scripts/dev.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+clear
+
+if bash ./scripts/build.sh dev; then
+  echo -e "\nBuild successful!\n"
+else
+  echo -e "\nBuild failed\n"
+  exit 1
+fi
+
+if [[ "${RUN_AFTER:-1}" == "1" ]]; then
+  echo "=========================="
+  echo "   Running CRUD "
+  echo "=========================="
+  ./build/dev/CRUD.exe
+else
+  echo "Skipping execution (-n passed)"
+fi
